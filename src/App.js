9d2903d6aef0cbda90637722a0e08e65cd8064a5
nomanme <abdullah.noman083@gmail.com>
2021-05-04 23:06:13 +0600

    contacts added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { Provider } from 'react-redux';
+import { Route, Router, Switch } from 'react-router';
+import AddContact from './components/contacts/AddContact';
+import Contacts from './components/contacts/Contacts';
+import EditContact from './components/contacts/EditContact';
+import { Navbar } from './components/elements/Navbar';
+import store from './store';
 import './styles/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Hello React</h2>
-      </header>
-    </div>
+    <Provider store={store}>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <div className="py-3">
+            <Switch>
+              <Route exact path="/" component={Contacts} />
+              <Route exact path="/contacts/add" component={AddContact} />
+              <Route
+                exact
+                path="/contacts/edit/:id"
+                component={EditContact}
+              />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
+  </Provider>
+   
   );
 }
 
--- src/components/elements/Navbar.js
+++ src/components/elements/Navbar.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-const Navbar = () => {
+export const Navbar = () => {
     return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default Navbar;
+        <nav className="navbar shadow fixed-top navbar-expand-sm navbar-dark bg-primary">
+        <div className="container">
+      <a href="#book" className="navbar-brand">Contact Book</a>
+          </div>
+       
+      </nav>
+    )
+}
